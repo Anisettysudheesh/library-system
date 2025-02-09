6fee12f4a6b5fd3b63b288dcacc94de5830e117d
Anisettysudheesh <anisettysudheesh@gmail.com>
2025-02-09 19:43:03 +0530

    Delete src/models/About.js

--- src/models/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-function About() {
-    return (
-    <div>
-        <h1 style={{textAlign:"center"}}>ABOUT PAGE</h1>
-    </div>  );
-}
-
-export default About;
